9b3d7c798b379eceadc92a9bb07befa6988d752c
BOBBER1997 <omaruco111@gmail.com>
2026-05-18 19:13:19 -0600

    cafeteria1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function Inscribirse(plan){
+    alert("En 5 minutos estara tu" + plan + " en tu mesa");
+}
+function entrenador(entrenador){
+    alert("La"+ entrenador + " es una buena eleccion para acompañar tu bebida")
+}
